3702a7b50bd8acd2cc77791c6b2390e586f6b516
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-04-25 23:36:45 -0400

    From HEX to RGB

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-function generateColor() {
-    let color = event.target;
-    color.classList.toggle('color');
+let letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+let randomIndex = Math.floor(Math.random() * letters.length);
+
+// let randomNumber = Math.floor(Math.random() * 10);
+
+// let randomLetter = letters[randomIndex];
+
+let hexNumber = [];
+
+const chooseValue = function () {
+    let value = Math.floor(Math.random() * 2) === 0;
+    if (value) {
+        return letters[Math.floor(Math.random() * letters.length)];
+    } else {
+        return Math.floor(Math.random() * 10);
+    }
+};
+
+for (let i = 0; i < 6; i++) {
+    hexNumber.push(chooseValue());
 }
+
+document.addEventListener('click', function (event) {
+    const block = event.target;
+
+    block.style.backgroundColor = 'rgb(20,300,40)';
+
+    // block.classList.toggle('color');
+});
+
+console.log(hexNumber);
+
+// function createHex() {
+//     let value = Math.floor(Math.random() * 2) === 0;
+//     if (value) {
+//         hexNumber.push(randomLetter);
+//     } else {
+//         hexNumber.push(randomNumber);
+//     }
+// }
