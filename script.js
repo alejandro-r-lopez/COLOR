a1a5304fa28855a3b73516884a0704c84fc7e352
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-04-26 00:13:56 -0400

    Working App

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-let letters = ['A', 'B', 'C', 'D', 'E', 'F'];
-let randomIndex = Math.floor(Math.random() * letters.length);
+function generateColor() {
+    let block = event.currentTarget;
 
-// let randomNumber = Math.floor(Math.random() * 10);
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
 
-// let randomLetter = letters[randomIndex];
-
-let hexNumber = [];
-
-const chooseValue = function () {
-    let value = Math.floor(Math.random() * 2) === 0;
-    if (value) {
-        return letters[Math.floor(Math.random() * letters.length)];
-    } else {
-        return Math.floor(Math.random() * 10);
-    }
-};
-
-for (let i = 0; i < 6; i++) {
-    hexNumber.push(chooseValue());
+    block.style.backgroundColor = `rgb(${red},${green},${blue})`;
 }
-
-document.addEventListener('click', function (event) {
-    const block = event.target;
-
-    block.style.backgroundColor = 'rgb(20,300,40)';
-
-    // block.classList.toggle('color');
-});
-
-console.log(hexNumber);
-
-// function createHex() {
-//     let value = Math.floor(Math.random() * 2) === 0;
-//     if (value) {
-//         hexNumber.push(randomLetter);
-//     } else {
-//         hexNumber.push(randomNumber);
-//     }
-// }
